9e7f497c047a35c5a7d69cfdcdb97d10b7526a55
unknown <prithiviraina1998@gmail.com>
2026-07-13 12:19:17 +0530

    Cors fixed

--- src/main/java/com/micro/auth/security/SecurityConfig.java
+++ src/main/java/com/micro/auth/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,6 +37,7 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
